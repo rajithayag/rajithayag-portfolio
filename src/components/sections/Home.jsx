f93506360b47a4d88bd7181d860a8dae418f23d3
Rajtiha Gunasekara <rajithayag@gmail.com>
2025-04-02 08:46:36 +1300

    Setting base url for images

--- src/components/sections/Home.jsx
+++ src/components/sections/Home.jsx
@@ -13,15 +13,15 @@ export const Home = () => {
                 <div className="grid grid-cols-1 md:grid-cols-3 gap-6 justify-center relative">
                                  
                     <a href="https://www.credly.com/badges/a75ea632-ad62-44e1-9567-18f47b11f29c/public_url" target="_blank" rel="nofollow">
-                    <img className="p-4 w-40 h-auto block mx-auto" src={`${import.meta.env.BASE_URL}/images/fundamentals-of-agile-project-management.png)`} alt="agile-project-management" title="Agile Project Management" />
+                    <img className="p-4 w-40 h-auto block mx-auto" src="https://rajithayag.github.io/rajithayag-portfolio/images/fundamentals-of-agile-project-management.png" alt="agile-project-management" title="Agile Project Management" />
                     </a>
 
                     <a href="https://www.credly.com/badges/5f81e7a5-39ef-43fe-b4bd-00bb129d0cfb/public_url" target="_blank" rel="nofollow">
-                    <img className="p-4 w-40 h-auto block mx-auto" src={`${import.meta.env.BASE_URL}/images/corporate-digital-communications.png)`} alt="corporate-digital-communications" title="Corporate Digital Communications" />
+                    <img className="p-4 w-40 h-auto block mx-auto" src="https://rajithayag.github.io/rajithayag-portfolio/images/corporate-digital-communications.png" alt="corporate-digital-communications" title="Corporate Digital Communications" />
                     </a>
 
                     <a href="https://www.credly.com/badges/a3dd4983-6252-4c1c-b992-decd328677f9/public_url" target="_blank" rel="nofollow">
-                    <img className="p-4 w-40 h-auto block mx-auto" src={`${import.meta.env.BASE_URL}/images/fundamentals-of-predictive-project-management.png)`} alt="predictive-project-management" title="Predictive Project Management" />
+                    <img className="p-4 w-40 h-auto block mx-auto" src="https://rajithayag.github.io/rajithayag-portfolio/images/fundamentals-of-predictive-project-management.png" alt="predictive-project-management" title="Predictive Project Management" />
                     </a>
 
                 </div>
